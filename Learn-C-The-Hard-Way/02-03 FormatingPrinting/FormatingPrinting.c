9ae327e64b57640eb8a71284edeb2e60c68ae4a9
Muhammad Ali <mu7ammad.3lii@gmail.com>
2023-07-25 21:07:21 +0300

    Added the second Exercise & the Third Exercise Combined

---
+++ Learn-C-The-Hard-Way/02-03 FormatingPrinting/FormatingPrinting.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc ,char *argv[])
+{
+	int age = 100;
+	float hight= 175.5;
+	printf("I am  %i years Old.\r\n",argv);
+	printf("I am %g cm tall.\n ",hight);
+	return 0;
+}
